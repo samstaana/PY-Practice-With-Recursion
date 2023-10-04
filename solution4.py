ed4d9cd94ab090372cdbca3ed3d238470f8f57c6
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-10-03 19:25:21 -0700

    solution 4

--- solution4.py
+++ solution4.py
@@ -1,1 +1,9 @@
 # Write code for algorithm 4 below
+def power (a, b):
+    if b < 1:
+        print('NO')
+    elif b == 1:
+        return a
+    else: return a * power(a, b - 1)
+
+print(power(2, 4))
