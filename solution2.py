21ed8902fdcf0cd7ce5fdcde6c01d7ec9e98cc27
Samantha Marie <sdurazo.UNLV@gmail.com>
2023-10-03 19:11:12 -0700

    solution 2

--- solution2.py
+++ solution2.py
@@ -1,1 +1,10 @@
 # Write code for algorithm 2 below
+
+def natural_numbers(n, i = 1):
+    if i > n:
+        return
+    else:
+        print(i)
+        natural_numbers(n, i + 1)
+
+natural_numbers(3)
